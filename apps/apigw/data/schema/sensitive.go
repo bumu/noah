eb4efed8f291f3f81dae4eb82e6e5293484db0eb
bumu <bumu@airdb.io>
2023-05-23 14:23:36 +0800

    feat: fix struct

--- apps/apigw/data/schema/sensitive.go
+++ apps/apigw/data/schema/sensitive.go
@@ -5,9 +5,10 @@ import "gorm.io/gorm"
 type Sensitive struct {
 	gorm.Model
 
-	Title  string
-	Vendor string // openai azure
-	Token  string
+	Categey     string
+	CountryCode string // country code 3 letters
+	Content     string
+	Remark      string
 }
 
 // TableName overrides the table name used by Key to `profiles`
